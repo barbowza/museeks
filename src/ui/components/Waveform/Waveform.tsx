b0a883081280355d1dd352b2711b79280b41dcda
Barbowza <barbowza@users.noreply.github.com>
2021-03-05 19:53:51 +0000

    Add <wip> Waveform component

---
+++ src/ui/components/Waveform/Waveform.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import styles from './Waveform.module.css';
+
+interface Props {
+  nothingYet?: string;
+}
+
+class Waveform extends React.Component<Props> {
+  nothingYet: string;
+  
+  constructor(props: Props) {
+    super(props);
+
+    this.nothingYet = props.nothingYet ?? 'nothing';
+  }
+
+
+  render() {
+    // const { somethingYouNeedFromProps } = this.props;
+
+    return (
+      <header className={styles.header}>
+        <div className={styles.header__mainControls}>
+          one
+        </div>
+        <div className={styles.header__playingBar}>
+          two
+        </div>
+        <div className={styles.header__search}>
+          three
+        </div>
+      </header>
+    );
+  }
+}
+
+// When you need state look at
+// const mapStateToProps = ({ player }: RootState) => ({
+// export default connect(mapStateToProps)(Header);
+export default Waveform;
