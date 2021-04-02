b7fdaadea47040574e2a7803ee8e8381ca5bd4a5
Barbowza <barbowza@users.noreply.github.com>
2021-04-02 17:06:45 +0100

    To act as proxy for HTMLAudioElement we must emit events

--- src/ui/lib/player-wavesurfer.ts
+++ src/ui/lib/player-wavesurfer.ts
@@ -1,20 +1,20 @@
-
+import { EventEmitter } from 'events';
 import WaveSurfer from 'wavesurfer.js';
 
-export default class PlayerWavesurfer {
+export default class PlayerWavesurfer extends EventEmitter {
   defaultPlaybackRate: number;
   playbackRate: number;
   volume: number;
   muted: boolean;
 
-
   private audio: HTMLAudioElement;
   private wavesurfer: WaveSurfer;
 
   constructor() {
+    super();
     this.audio = new Audio();
     this.wavesurfer = new WaveSurfer({
-      container: this.audio
+      container: this.audio,
     });
 
     this.defaultPlaybackRate = 1.0;
@@ -23,5 +23,19 @@ export default class PlayerWavesurfer {
     this.muted = false;
   }
 
-}
+  public addEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | AddEventListenerOptions
+  ): void {
+    return this.audio.addEventListener(type, listener, options);
+  }
 
+  public removeEventListener(
+    type: string,
+    listener: EventListenerOrEventListenerObject,
+    options?: boolean | EventListenerOptions
+  ): void {
+    return this.audio.removeEventListener(type, listener, options);
+  }
+}
